1549e287096d8b4cd70d829473edd957c3bc0dff
Soofa <youssefjoo2255@gmail.com>
2026-08-06 02:43:31 +0300

    adding a create operation

--- index.js
+++ index.js
@@ -2,29 +2,35 @@ const express = require ('express');
 
 const app = express();
 
+const {body, validationResult} = require('express-validator'); 
+
+app.use(express.json())
+
 const courses = [
     {
         id: 1, 
-        name: "JS", 
+        title: "JS", 
         price: 3000
     },
 
     {
         id: 2, 
-        name: "react", 
+        title: "react", 
         price: 5000
     },
 ]
 
 // CRUD 
 
+// get all courses
 app.get('/api/courses', (req, res, next) => {
     res.json(courses);
 
     next();
 })
 
-app.get('/api/courses/:courseId', (req, res, next) => {
+// get course 
+app.get('/api/courses/:courseId', (req, res) => {
 
     const courseId = +req.params.courseId; 
     const course = courses.find((course) => (course.id === courseId)); 
@@ -32,11 +38,44 @@ app.get('/api/courses/:courseId', (req, res, next) => {
     console.log(courseId)
 
     if(!course) {
-        res.status(404).json("Course Not found")
+        res.status(404).json({msg: "not found"})
     } 
 
     res.json(course)
 
+})
+
+// create course
+app.post('/api/courses', 
+    [body('title')
+        .notEmpty()
+        .withMessage("title is required")
+        .isLength({min: 2}).
+        withMessage('cannot be less than 2 digits'),
+
+    body('price')
+        .notEmpty()
+        .withMessage("title is required")
+        .isLength({min: 2}).
+        withMessage('cannot be less than 2 digits')]
+
+,(req, res, next) => {
+
+    const errors = validationResult(req);
+    console.log('errors: ', errors); 
+
+    if (!errors.isEmpty()){
+        res.status(400).json(errors.array());
+    }
+
+    console.log(req.body);
+
+    const course = {id: courses.length + 1, ...req.body};
+    courses.push(course);
+
+    res.status(201).json(course);
+
+    next()
 })
 
 
