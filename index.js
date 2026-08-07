48a90d30266396611d585edd1a03bb7f5f251ba8
Soofa <youssefjoo2255@gmail.com>
2026-08-07 21:59:49 +0300

    adding update and delete operations

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {body, validationResult} = require('express-validator');
 
 app.use(express.json())
 
-const courses = [
+let courses = [
     {
         id: 1, 
         title: "JS", 
@@ -78,6 +78,32 @@ app.post('/api/courses',
     next()
 })
 
+
+// Update Course
+
+app.patch('/api/courses/:courseId', (req, res) => {
+    const courseId = +req.params.courseId; 
+    const idx = courses.findIndex((c) => c.id === courseId);
+
+    if (idx === -1) return res.status(404).json({msg: "Not found"});
+
+    courses[idx] = {...courses[idx], ...req.body};
+    res.status(200).json(courses[idx]);
+
+
+})
+
+
+// Delete Course 
+
+app.delete('/api/courses/:courseId', (req, res) => {
+
+    const courseId = +req.params.courseId; 
+    courses = courses.filter((c) => c.id != courseId);
+
+    res.status(200).json({msg: 'done'})
+
+})
 
 app.listen(3000, () => {
     console.log('listen on port 3000')
