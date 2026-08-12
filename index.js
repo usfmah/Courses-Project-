6e83bd41432ecaff59aebb85e6a9205d72286550
Soofa <youssefjoo2255@gmail.com>
2026-08-12 18:37:36 +0300

    Add 404 middleware handler

--- index.js
+++ index.js
@@ -6,19 +6,26 @@ const app = express();
 
 const router = require('./routes/routes')
 
+const httpStatusText = require('./utils/httpStatusText');
+
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DB_URL).then(() => {
+    console.log('mongodb server started')
+  }).catch((err) => {
+    console.log('mongodb connection error: ', err.message);
+  });
 app.use(express.json())
 
 
 app.use('/api/courses', router)
 
 
- mongoose.connect(process.env.DB_URL).then(() => {
-    console.log('mongodb server started')
-  }).catch((err) => {
-    console.log('mongodb connection error: ', err.message);
-  });
+app.all('*', (req, res) => {
+      
+    return res.status(404).json({status: httpStatusText.ERROR, message: "This resourse is not avilable"});
+  
+})
 
 
 
