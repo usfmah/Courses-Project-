a6a7d2bf4d20c9324c082085d80e0e1fc7049d0a
Soofa <youssefjoo2255@gmail.com>
2026-08-16 01:31:03 +0300

    Adding user model to the project

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ app.use(express.json())
 app.use('/api/courses', router)
 
 
-app.all('*', (req, res, next) => {
-      
+app.use((req, res, next) => {
+    
     return res.status(404).json({status: httpStatusText.ERROR, message: "This resourse is not avilable"});
   
 });
 
 
-app.all((error, req, res, next) => {
+app.use((error, req, res, next) => {
         res.status(error.statusCode || 500).json({status: httpStatusText.ERROR, message: error.message})
 });
 
---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const validator = require('validator');
+
+const userSchema = new Schema ({
+
+    firstName: {
+        type: String,
+        required: true 
+    },
+
+    lastName: {
+        type: String,
+        required: true 
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validator.isEmail, 'It must be an email address']
+    },
+
+    password: {
+        type: String,
+        required: true 
+    },
+
+}, { versionKey: false })
+
+module.exports = mongoose.model('userSchema', userSchema);
